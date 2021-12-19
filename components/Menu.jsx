627a1d32b0a0bcdb57c1d94018c9c67b5a84fe15
aashankhan2981 <aashankhan2981@gmail.com>
2021-12-20 02:08:18 +0500

    menu updated

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -6,8 +6,8 @@ const Menu = ({toggle,setToggle,refer,refer2}) => {
         <div className={`${toggle?"grid absolute top-[70px] right-0 w-full bg-opacity-50 border-t-2 border-white bg-black  grid-rows-4 `":"hidden"} `}>
             <button className='hover:text-yellow-300 text-white' onClick={()=>{refer.current.refering() ; setToggle(!toggle)}}  >About Me</button>
             <button className='hover:text-yellow-300 text-white' onClick={()=>{refer2.current.getrefer(); setToggle(!toggle)}}>Skills</button>
-            <button className='hover:text-yellow-300 text-white' >Portfolio</button>
-            <button className='hover:bg-yellow-300 hover:text-white bg-white  cursor-pointer text-black px-4 py-2' >Contact Me</button>
+            <button className='hover:text-yellow-300 text-white'onClick={()=>{ setToggle(!toggle)}} >Portfolio</button>
+            <button className='hover:bg-yellow-300 hover:text-white bg-white  cursor-pointer text-black px-4 py-2'onClick={()=>{setToggle(!toggle)}} >Contact Me</button>
         </div>
     )
 }
