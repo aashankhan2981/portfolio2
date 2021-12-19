926bb70bc473c6e0e3d973769eacad7729a2892a
aashankhan2981 <aashankhan2981@gmail.com>
2021-12-20 02:06:20 +0500

    final

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -21,10 +21,10 @@ const Skills = forwardRef( (props,refer) => {
     return (
         <div className='flex px-5 flex-col  items-center pb-16'>
             <Button Title={"SKILLS"} refer={ref} />
-            <div className='flex flex-col px-5 md:items-center justify-center'>
+            <div className='flex flex-col px-5 mt-6 md:items-start  justify-center'>
             <div className='overflow-hidden  sm:mx-8 md:mx-12 eight:mx-32'>
-            <h2 className=' font-bold text-[28px]  '>USING NOW:</h2>
-            <div className='flex flex-wrap md:flex-wrap  space-x-10 sm:space-x-16  md:space-x-20 lg:space-x-28 flex-col sm:flex-row items-center justify-center mt-10'>
+            <h2 className=' font-bold md:text-left text-center text-[28px]  '>USING NOW:</h2>
+            <div className='flex flex-wrap  space-x-10 sm:space-x-16  md:space-x-20 lg:space-x-28 flex-col sm:flex-row items-center justify-center mt-10'>
            
             <Languages img={html} title={"HTML5"} mt={"ml-10 sm:ml-16 md:ml-20 lg:ml-0"} />
             <Languages img={css} title={"CSS3"} />
@@ -38,7 +38,7 @@ const Skills = forwardRef( (props,refer) => {
             </div>
             </div>
             <div className='overflow-hidden mt-9  sm:mx-8 md:mx-20 eight:mx-32'>
-            <h2 className=' font-bold text-[28px]    '>OTHER SKILLS:</h2>
+            <h2 className=' font-bold text-[28px] md:text-left text-center   '>OTHER SKILLS:</h2>
             <div className='flex xl:flex-nowrap flex-wrap space-x-10 sm:space-x-24 px-[30px] lg:space-x-28  flex-col sm:flex-row items-center justify-center mt-10'>
            
             <Languages img={html} title={"C"} mt={"ml-10 sm:ml-24 lg:ml-28 "} />
