98e61504af8de2aa0638ca488682239174dfef78
aashankhan2981 <aashankhan2981@gmail.com>
2021-12-20 01:59:10 +0500

    responsive

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -19,11 +19,12 @@ const Skills = forwardRef( (props,refer) => {
         getrefer
     }))
     return (
-        <div className='flex px-5 flex-col items-center pb-16'>
+        <div className='flex px-5 flex-col  items-center pb-16'>
             <Button Title={"SKILLS"} refer={ref} />
-            <div className='overflow-hidden  sm:mx-8 md:mx-20 eight:mx-32'>
+            <div className='flex flex-col px-5 md:items-center justify-center'>
+            <div className='overflow-hidden  sm:mx-8 md:mx-12 eight:mx-32'>
             <h2 className=' font-bold text-[28px]  '>USING NOW:</h2>
-            <div className='flex flex-wrap space-x-10 sm:space-x-16  md:space-x-20 lg:space-x-28 flex-col sm:flex-row items-center justify-center mt-10'>
+            <div className='flex flex-wrap md:flex-wrap  space-x-10 sm:space-x-16  md:space-x-20 lg:space-x-28 flex-col sm:flex-row items-center justify-center mt-10'>
            
             <Languages img={html} title={"HTML5"} mt={"ml-10 sm:ml-16 md:ml-20 lg:ml-0"} />
             <Languages img={css} title={"CSS3"} />
@@ -36,7 +37,7 @@ const Skills = forwardRef( (props,refer) => {
            
             </div>
             </div>
-            <div className='overflow-hidden mt-32  sm:mx-8 md:mx-20 eight:mx-32'>
+            <div className='overflow-hidden mt-9  sm:mx-8 md:mx-20 eight:mx-32'>
             <h2 className=' font-bold text-[28px]    '>OTHER SKILLS:</h2>
             <div className='flex xl:flex-nowrap flex-wrap space-x-10 sm:space-x-24 px-[30px] lg:space-x-28  flex-col sm:flex-row items-center justify-center mt-10'>
            
@@ -44,12 +45,8 @@ const Skills = forwardRef( (props,refer) => {
             <Languages img={css} title={"C++"} />
             <Languages img={tail} title={"PYTHON"} m={"mt-[21px]"} />
             <Languages img={wp} title={"Wordpress"} m={"mt-[7px]"} />
-            {/* <Languages img={js} title={"JAVASCRIPT"} m={"mt-[8px]"} /> */}
-            {/* <Languages img={r} title={"REACT(js)"} m={"mt-[7px]"} /> */}
-            {/* <Languages img={NEXT} title={"NEXT(js)"} m={"mt-[9px]"} /> */}
-
-            {/* <Languages img={git} title={"GITHUB"} m={"mt-[8px]"} /> */}
            
+            </div>
             </div>
             </div>
         </div>
